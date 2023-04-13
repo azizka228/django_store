07be945db2265b5195047a13a3a323feb72febf0
asd <asd>
2023-04-13 18:41:18 +0500

    added static files and templates

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("Hello world")
+    return render(request, "pages/index.html")
