51fb03257df69c51ee2c8133ceece5bca4132ceb
asd <asd>
2023-04-17 18:13:21 +0500

    added index and shop pages

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render, HttpResponse
 
 def home_view(request):
     return render(request, "pages/index.html")
+
+
+
+def shop_view(request):
+    return render(request, "pages/shop.html")
