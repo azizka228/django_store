a75de839258cd14d6fc99f32ef4b03874f45a252
asd <asd>
2023-04-27 18:31:44 +0500

    added funct for subcategory and product detail

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("categories/", views.shop_view, name="shop"),
+    path("categories/<slug:slug>/", views.subcategory_articles_view, name="subcategory_articles"),
+    path("product/<slug:slug>/", views.product_detail_view, name="product_detail")
 ]
+
